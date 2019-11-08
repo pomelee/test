5bb85bc29f863f199f6e4c6b206417b4c4feddc0
Lenna Lee <lenna@weid.kr>
2019-11-08 16:17:16 +0900

    import test teleop code.

--- run_teleoperation.sh
+++ run_teleoperation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install xautomation xdotool
+source ~/turtlebot3_ws/install/setup.bash
+export ROS_DOMAIN_ID=30
+export TURTLEBOT3_MODEL=burger
 
-ros2 run turtlebot3_teleop teleop_keyboard &
-
+ros2 run turtlebot3_teleop teleop_keyboard
