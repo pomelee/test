5d2a4de6c5d25bcc0cbe3caa2a06348ec9639407
Lenna Lee <lenna@weid.kr>
2019-11-13 14:46:48 +0900

    fixed

--- run_teleoperation.sh
+++ run_teleoperation.sh
@@ -5,5 +5,7 @@ source "/opt/ros/dashing/setup.bash"
 source ~/turtlebot3_ws/install/setup.bash
 export ROS_DOMAIN_ID=30
 export TURTLEBOT3_MODEL=burger
-exec "$@"
+
+#exec "$@"
+echo "start..."
 ros2 run turtlebot3_teleop teleop_keyboard
